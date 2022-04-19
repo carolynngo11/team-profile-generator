7533af87e31349f403c8e5b57b8b824f68fcf51f
Carolyn Ngo <carolynngo11@yahoo.com>
2022-04-18 23:50:30 -0500

    added bootstrap

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ inquirer
         name: 'Role',
         message: "Enter team member role.",
     },
+    {
+        type: 'input',
+        name: 'Email',
+        message: "Enter team member's Email.",
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: "Enter team member's GitHub.",
+    }
   ])
   .then((response) => {
     console.log(response)
@@ -29,7 +39,7 @@ inquirer
     })
   })
 
-  function generateHtml({Name, Role, Id, Email, Office}){
+  function generateHtml({Name, Role, Id, Email, Office, GitHub}){
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -40,22 +50,34 @@ inquirer
         <title>Document</title>
       </head>
       <body>
-          
+
+    <header style="block-size: 150px;
+                    margin-bottom: 80px;
+                    padding-top: 45px;
+                    text-align: center;
+                    color: white;
+                    background-color: rgb(235, 77, 98);">
+        <h1>My Team</h1>
+    </header>
+
         <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">${Name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${Role}</h6>
+        <div class="card-body bg-primary" style= "border-radius: 5px; box-shadow: 5px 5px 10px 2px rgba(119, 119, 119, 0.8);">
+        <h5 class="card-title text-white" style="font-size: x-large;">${Name}</h5>
+            <h6 class="card-subtitle mb-2 text-white">${Role}</h6>
+            <br>
             <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${Id}</li>
-            <li class="list-group-item">Email: ${Email}</li>
-            <li class="list-group-item">Office Number" ${Office}</li>
+            <li class="list-group-item">Email: <a href="URL PLACEHOLDER">${GitHub}</a></li>
+            <li class="list-group-item">Github: <a href="https://github.com/${GitHub}">${GitHub}</a></li>
+            <li class="list-group-item">Office Number: ${Office}</li>
           </ul>
         </div>
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-      </body>
+      
+    </body>
       </html>
 `
 }
